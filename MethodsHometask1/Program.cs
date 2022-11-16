f480da34d152a627c98e77eeaa5cb6331ecfb023
abdulayef1 <7ka1cy@.code.edu.az>
2022-11-17 00:09:32 +0400

    method hometask created

---
+++ MethodsHometask1/Program.cs
@@ -0,0 +1,99 @@
+﻿#region Task1
+//Task1
+//void resize(ref int[] arr,int size)
+
+//{	if (size<0)
+//	{
+//		Console.WriteLine("size cannot less than zero");
+//		return;
+//	}
+//    int[] newArr = new int[size];
+
+
+
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (i<arr.Length)
+//		{
+//			newArr[i]=arr[i];
+//		}
+
+//	}
+//	arr=newArr;	
+//}
+
+//int[] numbers = { 1, 2, 3, 4, 5 };
+
+//resize(ref numbers,-2);
+
+//foreach (var item in numbers)
+//{
+//	Console.WriteLine(item);
+//}
+
+//Console.WriteLine(numbers.Length);
+
+#endregion
+#region Task2
+
+//string Repaet(string word, int repationCount)
+//{
+//	if (repationCount < 1)
+//	{
+//		throw new Exception("repatation count cannot be less than 1\n\n");
+//	}
+//	string repatedWord = "";
+//	for (int i = 0; i < repationCount; i++)
+//	{
+//		repatedWord += word;
+//	}
+//	Console.WriteLine(repatedWord);// göndərilən word-ü göndərilən count qədər yan-yana yazdırıb geri qaytarsın.
+//								   //Bu cumleye gore console da yazdirib sora return eledim, eger deyilse idi ki, yan-yana yazilmish formasini geri qaytarsin conselda capa vermiyecekdim.
+
+//	return repatedWord;
+//}
+
+//Repaet("salam", -12);
+
+#endregion
+#region Task3
+
+
+
+//class Program
+//{
+//static double FindArea(double r)
+//{
+//    double p = 3;
+//    double s = (r * r) * p;
+
+
+//        return s;
+//}
+//static double FindArea(double a,double b)
+//{
+//        double  s= a * b;
+//        return s;
+//}
+//    static double FindArea(double r,double a, double b, double c)
+//{   
+//    double p = (a+b+c) / 2;
+//    double s=p*r;
+     
+//    return s;
+//}
+
+
+//    static void Main(string[] args)
+//    {
+
+//        Console.WriteLine(FindArea(3)); 
+//        Console.WriteLine(FindArea(3,3));
+//        Console.WriteLine(FindArea(3,3,3,3));
+       
+//    }
+//}
+
+#endregion
+
+
